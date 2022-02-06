61411da98b380f4a6cbaeefc22ed3e33c3f6c224
Wendy Rodriguez <wendyrod58@gmail.com>
2022-02-06 14:34:44 -0800

    added Bootstrap cards

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+
+// to check if open weather api is working
+var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=4bcf9a7748598d2f2a26f50816e3eaf2)"
+fetch(apiUrl);
+console.log("this is working");
+
+
