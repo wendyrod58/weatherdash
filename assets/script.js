0c16602bb4b5a7f8ef3bc23d77de568311c3fe4a
Wendy Rodriguez <wendyrod58@gmail.com>
2022-02-06 17:50:44 -0800

    added current conditions in page

--- assets/script.js
+++ assets/script.js
@@ -2,25 +2,18 @@ var apiKey = '4bcf9a7748598d2f2a26f50816e3eaf2';
 var searchTextEl = document.getElementById("search-text");
 var searchBtn = document.getElementById("getWeather");
 
-
-console.log("this is working");
-
-
 //function 
 function getWeatherAPI() {
-
-
     var geoCodingURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchTextEl.value + '&limit=1&appid=' + apiKey;
     console.log(geoCodingURL);
 
-    //Fetch THE LAt and LON based on t he user input 
+    //Fetch THE Lat and Lon based on the user input 
     fetch(geoCodingURL)
         .then(function (response) {
             //console.log("Api response", response);
-
             return response.json(); //on sucess give the json object 
-        }).
-        then(function (data) {
+        })
+        .then(function (data) {
             console.log("JSON object :", data);
             //Grab values from the JSON basically lat, lon and name 
             var long = data[0].lon;
@@ -37,7 +30,10 @@ function getWeatherAPI() {
                 .then(function (data1) {
                     console.log("current weather response", data1);
                     //display on HTMl PAGE 
-                    document.getElementById("current-temp").textContent = data1.current.temp; 
+                    document.getElementById("current-temp").textContent = data1.current.temp;
+                    document.getElementById("current-hum").textContent = data1.current.humidity;
+                    document.getElementById("current-wind").textContent = data1.current.wind_speed; 
+                    document.getElementById("current-uvi").textContent = data1.current.uvi;  
                 })
         })
        
@@ -45,5 +41,4 @@ function getWeatherAPI() {
 
 
 //Add Event listener 
-
 searchBtn.addEventListener('click', getWeatherAPI); 
