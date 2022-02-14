e3051f631b631e149825a03f78d4049d98f54151
Wendy Rodriguez <wendyrod58@gmail.com>
2022-02-13 16:00:02 -0800

    fixed url

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,7 @@ var searchHistory = []
 
 //function get the Weather
 function getWeatherAPI() {
-    var geoCodingURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchTextEl.value + '&limit=1&appid=' + apiKey;
+    var geoCodingURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + searchTextEl.value + '&limit=1&appid=' + apiKey;
     console.log(geoCodingURL);
 
     //Fetch the Lat and Lon based on the user input 
