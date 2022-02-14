da646814cd885f43a78454dea811367728c4bf56
Wendy Rodriguez <wendyrod58@gmail.com>
2022-02-13 16:58:41 -0800

    created search history in local storage

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,13 @@
 var apiKey = '4bcf9a7748598d2f2a26f50816e3eaf2';
 var searchTextEl = document.getElementById("search-text");
 var searchBtn = document.getElementById("getWeather");
-var searchHistory = []
+var searchHistory = JSON.parse(localStorage.getItem("cityList")); //convert it back to JSON obj  
 
+console.log("seacrh", searchHistory);
+if( searchHistory === null){
+    searchHistory = []; //if there is no value saved in local storage , for the first time assign it a blank array 
+}
+console.log("update search array ", searchHistory);
 
 
 //function get the Weather
@@ -39,7 +44,8 @@ function getWeatherAPI() {
                 .then(function (data1) {
                     console.log("current weather response", data1);
                     //display on HTMl PAGE 
-                    document.getElementById("current-date").textContent = data1.current.dt;
+                    var date = new Date(data1.current.dt * 1000).toLocaleDateString("en-US");
+                    document.getElementById("current-date").textContent = date;
                     document.getElementById("current-temp").textContent = data1.current.temp;
                     document.getElementById("current-hum").textContent = data1.current.humidity;
                     document.getElementById("current-wind").textContent = data1.current.wind_speed;
@@ -48,42 +54,50 @@ function getWeatherAPI() {
 
 
                     // //5-Day Forecast 
-                    // var forecastURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lati + "&lon=" + long + "&exclude=hourly,minutely,alerts&units=imperial&appid=" + apiKey;
-                    // console.log(forecastURL);
 
-                    //fetch Request for daily weather api 
-                    // fetch(forecastURL)
-                    //     .then(function (response1) {
-                    //         return response1.json();
-                    //     })
-                    //     .then(function (data1) {
-                    //         console.log("daily weather response", data1);
                     // display in HTML page
-                    document.getElementById("day1-date").textContent = data1.daily[0].dt;
-                    document.getElementById("day1-temp").textContent = data1.daily[0].temp.day;
-                    document.getElementById("day1-hum").textContent = data1.daily[0].humidity;
-                    document.getElementById("day1-wind").textContent = data1.daily[0].wind_speed;
-                    document.getElementById("day1-uvi").textContent = data1.daily[0].uvi;
+                    var date1 = new Date(data1.daily[1].dt * 1000).toLocaleDateString("en-US");
+                    document.getElementById("day1-date").textContent = date1;
+                    document.getElementById("day1-temp").textContent = data1.daily[1].temp.day;
+                    document.getElementById("day1-hum").textContent = data1.daily[1].humidity;
+                    document.getElementById("day1-wind").textContent = data1.daily[1].wind_speed;
+                    document.getElementById("day1-uvi").textContent = data1.daily[1].uvi;
 
-                    document.getElementById("day2-temp").textContent = data1.daily[1].temp.day;
-                    document.getElementById("day2-hum").textContent = data1.daily[1].humidity;
-                    document.getElementById("day2-wind").textContent = data1.daily[1].wind_speed;
-                    document.getElementById("day2-uvi").textContent = data1.daily[1].uvi;
 
-                    document.getElementById("day3-temp").textContent = data1.daily[2].temp.day;
-                    document.getElementById("day3-hum").textContent = data1.daily[2].humidity;
-                    document.getElementById("day3-wind").textContent = data1.daily[2].wind_speed;
-                    document.getElementById("day3-uvi").textContent = data1.daily[2].uvi;
+                    var date2 = new Date(data1.daily[2].dt * 1000).toLocaleDateString("en-US");
+                    document.getElementById("day2-date").textContent = date2;
+                    document.getElementById("day2-temp").textContent = data1.daily[2].temp.day;
+                    document.getElementById("day2-hum").textContent = data1.daily[2].humidity;
+                    document.getElementById("day2-wind").textContent = data1.daily[2].wind_speed;
+                    document.getElementById("day2-uvi").textContent = data1.daily[2].uvi;
 
-                    document.getElementById("day4-temp").textContent = data1.daily[3].temp.day;
-                    document.getElementById("day4-hum").textContent = data1.daily[3].humidity;
-                    document.getElementById("day4-wind").textContent = data1.daily[3].wind_speed;
-                    document.getElementById("day4-uvi").textContent = data1.daily[3].uvi;
+                    var date3 = new Date(data1.daily[3].dt * 1000).toLocaleDateString("en-US");
+                    document.getElementById("day3-date").textContent = date3;                    
+                    document.getElementById("day3-temp").textContent = data1.daily[3].temp.day;
+                    document.getElementById("day3-hum").textContent = data1.daily[3].humidity;
+                    document.getElementById("day3-wind").textContent = data1.daily[3].wind_speed;
+                    document.getElementById("day3-uvi").textContent = data1.daily[3].uvi;
 
-                    document.getElementById("day5-temp").textContent = data1.daily[4].temp.day;
-                    document.getElementById("day5-hum").textContent = data1.daily[4].humidity;
-                    document.getElementById("day5-wind").textContent = data1.daily[4].wind_speed;
-                    document.getElementById("day5-uvi").textContent = data1.daily[4].uvi;
+                    var date4 = new Date(data1.daily[4].dt * 1000).toLocaleDateString("en-US");
+                    document.getElementById("day4-date").textContent = date4;             
+                    document.getElementById("day4-temp").textContent = data1.daily[4].temp.day;
+                    document.getElementById("day4-hum").textContent = data1.daily[4].humidity;
+                    document.getElementById("day4-wind").textContent = data1.daily[4].wind_speed;
+                    document.getElementById("day4-uvi").textContent = data1.daily[4].uvi;
+
+                    var date5 = new Date(data1.daily[5].dt * 1000).toLocaleDateString("en-US");
+                    document.getElementById("day5-date").textContent = date5;             
+                    document.getElementById("day5-temp").textContent = data1.daily[5].temp.day;
+                    document.getElementById("day5-hum").textContent = data1.daily[5].humidity;
+                    document.getElementById("day5-wind").textContent = data1.daily[5].wind_speed;
+                    document.getElementById("day5-uvi").textContent = data1.daily[5].uvi;
+
+                    //append value to the array 
+                    searchHistory.push(data[0].name);
+                    //save the city in localstoage 
+                    localStorage.setItem("cityList", JSON.stringify(searchHistory));
+                    //calling the function 
+                    searchedCities();
                 })
         })
 
@@ -94,6 +108,26 @@ function getWeatherAPI() {
 // loop
 // create an array
 
+function searchedCities() {
+    document.getElementById("searched-cities").innerHTML = ""; //empty the previous values 
+
+    for (var index = 0; index < searchHistory.length; index++) {
+        console.log("city name", searchHistory[index]);
+        //create an li tag eleemnt 
+        var liTag = document.createElement("li"); 
+        //set the display text of city name 
+        liTag.textContent = searchHistory[index]; 
+
+        liTag.setAttribute("class","list-group-item"); 
+        console.log(liTag);
+
+        //append it to the ul , so that it displays on my HTML page 
+        document.getElementById("searched-cities").append(liTag); 
+    }
+}
+
+//calling the function on page load 
+searchedCities();
 
 //Add Event listener 
 searchBtn.addEventListener('click', getWeatherAPI); 
